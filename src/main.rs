d92c17b1d5681612f6b2b90a8f9ff3277cc5e66b
Kyle Lacy <kylelacy+git@pm.me>
2021-11-23 12:49:43 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use axum::{handler::Handler, routing};
+
+#[tokio::main]
+async fn main() {
+    let fizz = axum::Router::new()
+        .route("/buzz", routing::get(|| async { "FizzBuzz" }))
+        .fallback(fizz_fallback.into_service());
+
+    let router = axum::Router::new()
+        .route("/foo", routing::get(|| async { "Foo" }))
+        .route("/bar", routing::get(|| async { "Bar" }))
+        .nest("/fizz", fizz)
+        .fallback(top_level_fallback.into_service());
+
+    axum::Server::bind(&"0.0.0.0:3333".parse().unwrap())
+        .serve(router.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn top_level_fallback() -> impl axum::response::IntoResponse {
+    "Top level fallback"
+}
+
+async fn fizz_fallback() -> impl axum::response::IntoResponse {
+    "Fizz fallback"
+}
